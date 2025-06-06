e0a6675782c925e033b005a2e3de91682fc89155
Πita <154020297+pita092@users.noreply.github.com>
2025-06-06 23:58:11 +0300

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMonoNL Nerd Font:style=Regular:size=12:antialias=true:autohint=true";
+static char *font = "JetBrainsMonoNL Nerd Font:style=Regular:size=13:antialias=true:autohint=true";
 
 static int borderpx = 0;
 

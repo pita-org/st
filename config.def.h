65ea653350352ccba0a7bad5f393335b4c5048ac
petah <154020297+pita092@users.noreply.github.com>
2025-06-21 16:33:16 +0300

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "JetBrainsMonoNL Nerd Font:size=13:autohint=true";
-static char *font = "Rubik:size=13:antialias=true:autohint=true";
+static char *font = "Rubik:size=13";
 
 
 
